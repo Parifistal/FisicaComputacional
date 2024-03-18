2dd3e270ea46455803f64b9e393cce0cbc6b8329
Parifistal <autoesteger@gmail.com>
2024-03-18 15:25:12 -0500

    Segunda modificación, se reestructuro el codigo inicial eliminando algunas partes del anterior, paralelamente de creo un archivo copia donde poder ver el codigo antiguo antes de borrar las partes modificadas

--- 18Marz/Cuadratura.cpp
+++ 18Marz/Cuadratura.cpp
@@ -2,45 +2,51 @@
 #include<cmath>
 #include<fstream>
 
+//Funcion para integrar
+double funcion(double x){
+    return 100*pow(x,2)*cos(20*x);
+
+}
+
+// Calculo de la integral aproximada bajo cuadratura rectangular
+
+double cuadraturaRectangular(double a, double b, int N){
+    double h=(b-a)/N;
+    double suma=0.0;
+    for(int i=0;i<N;i++){
+        double xi=a+i*h;
+        suma+=funcion(xi);
+    }
+    return h*suma;
+}
+
+
 int main(){
-    std::ofstream datafile("resultados.dat");
+    std::ofstream datafile("resultados_cuadratura_rectangular.dat");
 
     for(int N=1; N<=10000; ++N){
-        double integral=0.0; //Iniciando el valor de la integral.
-        double a=0.0;  //Limite inferior.
-        double b=1.0;   //Limite superior.
-        double h=(b-a)/N;   //Ancho del paso
-        for(int i=0;i<N;++i){
-            double xi=a+(i+0.5)*h;  //Calculamos el punto medio del sub-intervalo.
-            integral+=100*std::pow(xi,2)*std::cos(20*xi);
-        }
-        integral*=h;    //Multiplicar por el ancho del sub-intervalo.
+        double integral= cuadraturaRectangular(0.0,1.0,N);
         double integral_exacta=4.7459;
         datafile<<N<<" "<<integral<<" "<<integral_exacta<<std::endl;
-    }
+        }
+    
+    
     datafile.close();
 
-    std::ofstream scriptFile("grafico_integral_aproximada.gp");
+    std::ofstream scriptFile("grafico_integral_vs_cuadratura_rectangular.gp");
     scriptFile<<"set term png\n";
-    scriptFile<<"set output 'grafico_integral_aproximada.png' \n";
+    scriptFile<<"set output 'grafico_integral_vs_cuadratura_rectangular.png' \n";
     scriptFile<<"set xlabel 'N'\n";
     scriptFile<<"set ylabel 'Integral' \n";
     scriptFile<<"set logscale x\n";
-    scriptFile<<"plot 'resultados.dat' u 1:2 w l title 'Integral aproximada' \n";
+    scriptFile<<"plot 'resultados_cuadratura_rectangular.dat' u 1:2 w l title 'Integral aproximada', '' u 1:3 w l title 'Valor exacto' \n";
     scriptFile.close();
 
-    std::ofstream scriptFile2("grafico_integral_exacta.gp");
-    scriptFile2<<"set term png\n";
-    scriptFile2<<"set output 'grafico_integral_exacta.png' \n";
-    scriptFile2<<"set xlabel 'N'\n";
-    scriptFile2<<"set ylabel 'Integral' \n";
-    scriptFile2<<"set logscale x\n";
-    scriptFile2<<"plot 'resultados.dat' u 1:3 w l title 'Valor exacto de la integral' \n";
-    scriptFile2.close();
+
 
 //Ejecutar Gnuplot
-system("gnuplot grafico_integral_aproximada.gp");
-system("gnuplot grafico_integral_exacta.gp");
+system("gnuplot grafico_integral_vs_cuadratura_rectangular.gp");
+system("gnuplot grafico_integral_vs_cuadratura_rectangular.gp");
 
 return 0;
 
